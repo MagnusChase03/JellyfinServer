c4ae0fc7e9f33a837aab8e73edafff8ab6808a17
Zachary <zachness2003@gmail.com>
2022-10-05 22:48:24 -0500

    Init

---
+++ jellyfin/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -fsSL https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/jellyfin.gpg
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | tee /etc/apt/sources.list.d/jellyfin.list
